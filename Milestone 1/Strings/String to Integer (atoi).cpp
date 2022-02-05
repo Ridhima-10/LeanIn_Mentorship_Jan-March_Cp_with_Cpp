6bada40a7c04fe05067a424b878f1c0848b9e01f
Ridhima Misra <77698950+Ridhima-10@users.noreply.github.com>
2022-02-05 17:32:08 +0530

    Create String to Integer (atoi).cpp

---
+++ Milestone 1/Strings/String to Integer (atoi).cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+		int n=s.length(),i=0;
+		bool flag=true;
+		
+		while(i<n and s[i]==' ')
+            i++;
+		
+		if(s[i]=='+') 
+            i++;
+		else if(s[i]=='-')
+        {
+			flag=false;
+			i++;
+		}
+		
+		long long int res=0;
+		while(i<n)
+        {
+			if(s[i]>='0' and s[i]<='9')
+            {
+				res=res*10+(s[i]-'0');
+				if(res>=INT_MAX) break;
+		    }
+			else if((s[i]=='+' or s[i]=='-') and res==0)
+                return 0;
+			else 
+                break;
+			i++;
+		}
+		
+		if(!flag)
+            res*=-1; 
+		
+		if(res<=INT_MIN)
+            return INT_MIN;
+		else if(res>=INT_MAX)
+            return INT_MAX;
+        
+        
+		return res;
+	}
+};
